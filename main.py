4d1049ea928cdfa7704489451ff7f99026b0c344
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2024-12-21 21:26:20 -0500

    pixel characters (basic)

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ Pygame! combining asteroids with space invaders
 import pygame
 
 pygame.init() # initialize modules
+clock = pygame.time.Clock() # create clock object
 
 screen_width, screen_height = pygame.display.Info().current_w, pygame.display.Info().current_h # fullscreen
 
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE) # can change size
 
-pygame.display.set_caption('Space Shooter')
+pygame.display.set_caption('Space Shooter') # title
+
+BG_COLOR = pygame.Color(10, 10, 10) # bg color
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # check for X out of game
+            run = False
+    screen.fill(BG_COLOR)
+    pygame.display.flip()  # update window
+    clock.tick(60)  # 60 fps
+
+
+
+
